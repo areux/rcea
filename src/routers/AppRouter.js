b62b16d6bf7945d1bc25db92694b6fdf727d5e0b
Arash <x@nix>
2020-10-03 21:29:40 +0330

    .

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -5,10 +5,10 @@ import { createBrowserHistory } from 'history';
 import AddExpensePage from '../components/AddExpensePage';
 import ExpenseDashboardPage from '../components/ExpenseDashboardPage';
 import EditExpensePage from '../components/EditExpensePage';
-import HelpPage from '../components/HelpPage';
 import NotFoundPage from '../components/NotFoundPage';
 import LoginPage from '../components/LoginPage';
 import PrivateRoute from './PrivateRoute';
+import PublicRoute from './PublicRoute';
 
 console.log('Loading AppRouter...');
 export const history = createBrowserHistory();
@@ -18,11 +18,10 @@ const AppRouter = () => {
         <Router history={history}>
                 <div>
                     <Switch>
-                        <Route exact path="/" component={LoginPage} />
+                        <PublicRoute exact path="/" component={LoginPage} />
                         <PrivateRoute path="/dashboard" component={ExpenseDashboardPage} />
                         <PrivateRoute path="/create" component={AddExpensePage} />
                         <PrivateRoute path="/edit/:id" component={EditExpensePage} />
-                        <Route path="/help" component={HelpPage} />
                         <Route component={NotFoundPage} />
                     </Switch>
                 </div>
